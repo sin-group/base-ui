ea497f6c659efcc2668c36c4de72ec613ae97b2c
Kyles Light <kuilin@yangqianguan.com>
2018-04-12 14:07:35 +0800

    [base-ui] Minor change in action style
    Summary: as title
    
    Test Plan: none
    
    Reviewers: bogeng, panezhang, smwang, weisun, xinzhong, dmhao, xymo, qingliu, jingyang, yanglan, wenxiujiang, xiaodongyu, zhenkunliu
    
    Reviewed By: qingliu
    
    Differential Revision: https://code.yangqianguan.com/D37903

--- demo/src/app/css-framework/component/css-element/css-element.vue
+++ demo/src/app/css-framework/component/css-element/css-element.vue
@@ -13,18 +13,21 @@
                 <div class="action-group">
                     <div class="action-header">Collection <button>管理</button></div>
 
-                    <button class="action-bar">Mock (3) <i class="b-icon-right"/></button>
-                    <a href="#" class="action-bar">Mayuri (10) <i class="b-icon-right"/></a>
+                    <button class="action-bar">Mock (3) <i class="b-icon-right"></i></button>
+                    <a href="#" class="action-bar">Mayuri (10) <i class="b-icon-right"></i></a>
                     <a href="#" class="action-bar">Create Collection</a>
                 </div>
 
                 <div class="action-group">
                     <div class="action-header">Api <button>管理</button></div>
 
-                    <button class="action-bar">Subscribe <i class="b-icon-love"/></button>
-                    <button class="action-bar">Add to collection<i class="b-icon-create"/></button>
-                    <button class="action-bar">View detail <i class="b-icon-detail"/></button>
-                    <button class="action-bar">More operation <i class="b-icon-more"/></button>
+                    <button class="action-bar">
+                        Subscribe <i class="b-icon-love"></i>
+                        <span class="action-detail">You will receive update email after subscribing</span>
+                    </button>
+                    <button class="action-bar">Add to collection<i class="b-icon-create"></i></button>
+                    <button class="action-bar">View detail <i class="b-icon-detail"></i></button>
+                    <button class="action-bar">More operation <i class="b-icon-more"></i></button>
                 </div>
             </div>
         </div>
